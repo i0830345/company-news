91f224cf0ce9bb34a97406cbfeaeddfb87ceb121
i0830345 <56945999+i0830345@users.noreply.github.com>
2019-10-25 14:55:54 +0900

    Update MainActivity.cs

--- CompanyNews.Android/MainActivity.cs
+++ CompanyNews.Android/MainActivity.cs
@@ -25,9 +25,9 @@ namespace CompanyNews.Droid
 
             base.OnCreate(savedInstanceState);
 
-            // APPCENTER
-            //AppCenter.Start("92209e19-94f7-4b7d-a72d-be5695cb5788",
-            //       typeof(Analytics), typeof(Crashes), typeof(Push), typeof(Distribute));
+            //APPCENTER
+            AppCenter.Start("4c495b8b-10e4-470e-bc58-69c3342e7594",
+                   typeof(Analytics), typeof(Crashes), typeof(Push), typeof(Distribute));
 
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
             FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
